06875ebbcf8af0ffb2e67ae4a020df62de1e506a
michael-takyi23 <>
2023-08-18 12:53:41 +0000

    Update images folder in assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,8 @@ function playGame(choice) {
     document.getElementById('ComputerChoice').textContent = "Opponent's Choice: " + computerChoice;
     document.getElementById('duelOutcome').textContent = result;
 
-    document.getElementById('playerChoiceImage').src = `assets/images/${choice}.png`;
-    document.getElementById('computerChoiceImage').src = `assets/images/${computerChoice}.png`;
+    document.getElementById('playerChoiceImage').src = `assets/images/${choice}.jpg`;
+    document.getElementById('computerChoiceImage').src = `assets/images/${computerChoice}.jpg`;
 
     if (result === 'You Prevailed!') {
         score++;
@@ -33,4 +33,79 @@ function playGame(choice) {
         alert('Quantum Duel Ends! Your quantum points are: ' + score);
         resetGame();
     }
+}
+
+function updatePlayerHistory(choice) {
+    playerHistory[choice]++;
+}
+
+function makeAdaptiveComputerChoice() {
+    const choices = Object.keys(playerHistory);
+    let prediction = choices[0];
+
+    for (let i = 1; i < choices.length; i++) {
+        if (playerHistory[choices[i]] > playerHistory[prediction]) {
+            prediction = choices[i];
+        }
+    }
+
+    return counterMove(prediction);
+}
+
+function counterMove(prediction) {
+    switch (prediction) {
+        case 'quantumRock':
+            return 'photonPaper';
+        case 'photonPaper':
+            return 'superpositionScissors';
+        case 'superpositionScissors':
+            return 'entangledLizard';
+        case 'entangledLizard':
+            return 'quantumRock';
+        case 'spockWaveFunction':
+            return 'photonPaper';
+        default:
+            const choices = ['quantumRock', 'photonPaper', 'superpositionScissors', 'entangledLizard', 'spockWaveFunction'];
+            return choices[Math.floor(Math.random() * choices.length)];
+    }
+}
+
+function determineWinner(playerChoice, computerChoice) {
+    let outcomeElement = document.getElementById('duelOutcome');
+
+    if (playerChoice === computerChoice) {
+        outcomeElement.style.color = "yellow"; 
+        return 'Quantum Stalemate!';
+    }
+    if ((playerChoice === 'quantumRock' && (computerChoice === 'superpositionScissors' || computerChoice === 'entangledLizard')) ||
+        (playerChoice === 'photonPaper' && (computerChoice === 'quantumRock' || computerChoice === 'spockWaveFunction')) ||
+        (playerChoice === 'superpositionScissors' && (computerChoice === 'photonPaper' || computerChoice === 'entangledLizard')) ||
+        (playerChoice === 'entangledLizard' && (computerChoice === 'spockWaveFunction' || computerChoice === 'photonPaper')) ||
+        (playerChoice === 'spockWaveFunction' && (computerChoice === 'quantumRock' || computerChoice === 'superpositionScissors'))) {
+        
+        outcomeElement.style.color = "lime";
+        return 'You Prevailed!';
+    }
+
+    outcomeElement.style.color = "red";
+    return 'Quantum Defeat!';
+}
+
+function resetGame() {
+    score = 0;
+    rounds = 3;
+    playerHistory = {
+        quantumRock: 0,
+        photonPaper: 0,
+        superpositionScissors: 0,
+        entangledLizard: 0,
+        spockWaveFunction: 0
+    };
+    document.getElementById('quantumPoints').textContent = score;
+    document.getElementById('duelRounds').textContent = rounds;
+    document.getElementById('playerChoice').textContent = '';
+    document.getElementById('ComputerChoice').textContent = '';
+    document.getElementById('duelOutcome').textContent = '';
+    document.getElementById('playerChoiceImage').src = 'assets/images/rpsls.png';
+    document.getElementById('computerChoiceImage').src = 'assets/images/rpsls.png';
 }
