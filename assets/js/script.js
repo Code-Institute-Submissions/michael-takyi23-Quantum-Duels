e55230b6a18b7d5ab40a342b2b34f5d161ab1d67
michael-takyi23 <>
2023-08-22 12:46:24 +0000

    Update script.js, initialize game variables for score, rounds and player history

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let score = 0;
+let rounds = 3;
+let playerHistory = {
+    quantumRock: 0,
+    photonPaper: 0,
+    superpositionScissors: 0,
+    entangledLizard: 0,
+    spockWaveFunction: 0
+};
